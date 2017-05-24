4f42c1b90296e56a4c96a3167cc5d561ac8918c1
jeanfuentesn <jeanfuentesn@gmail.com>
2017-05-23 23:22:18 -0400

    registrosiniestroWEB_Jean
    coloque /callcenter/blablabla a todas las url (js y controller) , el
    anterior me equivoqué y no le puse nombre, pero es el mismo, solo que
    este tiene un comentario para poder hacer un cambio.

--- registrosiniestroweb/src/main/java/cl/duoc/controller/CallCenterController.java
+++ registrosiniestroweb/src/main/java/cl/duoc/controller/CallCenterController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.websocket.server.PathParam;
 import java.util.List;
-
+//hola
 @Controller
 public class CallCenterController {
 
